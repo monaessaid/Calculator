6b77ec9088a2506d3056a11abc36d7a0dd496f11
Mona <2829922e@student.gla.ac.uk>
2024-09-03 19:17:07 -0700

    Calculator Panel

---
+++ calculator_front-end/src/CalculatorPanel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CalculatorButton from './CalculatorButton';
+
+function CalculatorPanel(props) {
+
+    const captions = [
+        "7", "8", "9", "/",
+        "4", "5", "6", "*",
+        "1", "2", "3", "-",
+        "0", ".", "C", "+",
+        "="
+    ];
+
+    
+
+}
